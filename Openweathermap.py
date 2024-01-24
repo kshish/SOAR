d56cc06b6c624996a44d53ee43b574715648b08c
soardev <soardev>
2024-01-24 00:00:31 +0000

    test OWM apis

--- Openweathermap.py
+++ Openweathermap.py
@@ -21,7 +21,7 @@ def on_start(container):
 def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("format_1() called")
 
-    template = """?appid=e3c29ce408f5a2010d8b8332fc8adcc8&lon=41&lat=10\n"""
+    template = """?appid=e3c29ce408f5a2010d8b8332fc8adcc8&lon=41&lat=10&units=imperial\n"""
 
     # parameter list for template variable replacement
     parameters = [
@@ -70,7 +70,60 @@ def get_data_1(action=None, success=None, container=None, results=None, handle=N
     ## Custom Code End
     ################################################################################
 
-    phantom.act("get data", parameters=parameters, name="get_data_1", assets=["openweathermap "])
+    phantom.act("get data", parameters=parameters, name="get_data_1", assets=["openweathermap "], callback=format_2)
+
+    return
+
+
+@phantom.playbook_block()
+def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_2() called")
+
+    template = """Weather info for {0} at {2}\nTemperature: {1}\nHumidity: {3}\nPressure: {4}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "get_data_1:action_result.data.*.parsed_response_body.name",
+        "get_data_1:action_result.data.*.parsed_response_body.main.temp",
+        "get_data_1:action_result.data.*.parsed_response_body.dt",
+        "get_data_1:action_result.data.*.parsed_response_body.main.humidity",
+        "get_data_1:action_result.data.*.parsed_response_body.main.pressure"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_2")
+
+    add_comment_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_comment_1() called")
+
+    format_2 = phantom.get_format_data(name="format_2")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment=format_2)
 
     return
 
