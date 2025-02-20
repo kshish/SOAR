9574185791a6a9936b18263753cd3aec4a11d788
soardev <soardev>
2025-02-20 20:06:57 +0000

    capabilities

--- test splunk http.py
+++ test splunk http.py
@@ -27,7 +27,7 @@ def get_data_1(action=None, success=None, container=None, results=None, handle=N
 
     parameters.append({
         "headers": "",
-        "location": "search/jobs",
+        "location": "authorization/capabilities",
         "verify_certificate": False,
     })
 
