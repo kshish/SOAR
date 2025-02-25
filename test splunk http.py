cd189af0b7a368759e544b98db81843de74c23e3
soardev <soardev>
2025-02-25 20:23:30 +0000

    config

--- test splunk http.py
+++ test splunk http.py
@@ -27,7 +27,7 @@ def get_data_1(action=None, success=None, container=None, results=None, handle=N
 
     parameters.append({
         "headers": "",
-        "location": "storage/collections/data",
+        "location": "storage/collections/config",
         "verify_certificate": False,
     })
 
